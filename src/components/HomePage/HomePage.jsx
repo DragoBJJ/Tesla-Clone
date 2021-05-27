67d813f3fd58e0bd3051c8f0e5851f2b881ca49c
Jakub Pawelski <j.pawelski@interia.pl>
2021-05-27 13:31:06 +0200

    Create teslaData file

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,58 +1,7 @@
-import React from "react";
 import { Container } from "./styledHomePage";
-
 import Section from "../Section/Section";
 
-const teslaData = [
-  {
-    title: "Model S5",
-    description: "Order online for Touchless Deliver ",
-    img: "model-s.jpg",
-    leftBtn: "customOrder",
-    rightBtn: "Existing inventory"
-  },
-  {
-    title: "Model Y",
-    description: "Order online for Touchless Deliver ",
-    img: "model-y.jpg",
-    leftBtn: "customOrder",
-    rightBtn: "Existing inventory"
-  },
-  {
-    title: "Model 3",
-    description: "Order online for Touchless Deliver ",
-    img: "model-3.jpg",
-    leftBtn: "customOrder",
-    rightBtn: "Existing inventory"
-  },
-  {
-    title: "Model X",
-    description: "Order online for Touchless Deliver ",
-    img: "model-X.jpg",
-    leftBtn: "customOrder",
-    rightBtn: "Existing inventory"
-  },
-  {
-    title: "Lowest Cost Solar Panels in America",
-    description: "Money back guarantee",
-    img: "solar-panel.jpg",
-    leftBtn: "Order now",
-    rightBtn: "Learn More"
-  },
-  {
-    title: "Solar for new Roofs",
-    description: "Solar Roof Costs Less Then a New",
-    img: "solar-roof.jpg",
-    leftBtn: "customOrder",
-    rightBtn: "Existing inventory"
-  },
-  {
-    title: "Accessories",
-    description: "",
-    img: "accessories.jpg",
-    leftBtn: "Shop Now"
-  }
-];
+import { teslaData } from "./teslaData";
 
 const HomePage = () => {
   return (
